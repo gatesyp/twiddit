6f1b1059e1bf8140dd87e57ed46ef499bd980d16
gatesyp <ig11@zips.uakron.edu>
2016-01-15 17:23:28 -0500

    Changed program to use new tweepyCursor object Downloads ALL tweets of a user instead of specified amount, pipes it to a .txt file for later use TODO - pipe .txt file to AlchemyAPI and parse results

--- twitterAgent.py
+++ twitterAgent.py
@@ -1,9 +1,11 @@
 # use: python twitterAgent.py <twitterID> <#oftweetswanted>
 from __future__ import absolute_import, print_function
 
+import codecs
 import sys
 import tweepy
-
+import json
+import pprint
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -15,16 +17,19 @@ consumer_secret="vz2IRxjqSUAyiGIqAvFHMtHN3DzcGt3DHl2LnI6zSGwXvP3rqG"
 access_token="378220962-EZjtEVdg9DVvIt9FqfgiJw38j8iLnAFyc0cGOrnU"
 access_token_secret="RJm3zc30b3hq4TdQLS1WyLW194lLfLINLKqo8zwCsKOfo"
 
+pp = pprint.PrettyPrinter(indent=4)
+
+def store(tweet, profile):
+	global pp
+	fp = codecs.open(profile + ".txt", "a", "utf-8")
+	fp.write(tweet["text"])
+
 # using OAuth to validate connection
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.secure = True
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
-# use first argvar for the Twitter ID, second argvar is how many tweets you want returned
-timeline = api.user_timeline(sys.argv[1], include_rts=True, count=sys.argv[2])
-for tweet in timeline:
-#	print("ID:", tweet.id)
-#	print("User ID:", tweet.user.id)
-	print("Text:", tweet.text.encode('UTF-8'))
+
+# use first argvar for the Twitter ID
+for tweet in tweepy.Cursor(api.user_timeline, id=sys.argv[1]).items():
+	store(tweet._json, sys.argv[1])
 
