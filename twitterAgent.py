829cf835955ab76b88ac1553d39fd9a401ba04c4
gatesyp <ig11@zips.uakron.edu>
2016-01-15 19:15:24 -0500

    created seperate module to handle the authentication process

--- twitterAgent.py
+++ twitterAgent.py
@@ -1,6 +1,7 @@
 # use: python twitterAgent.py <twitterID> <#oftweetswanted>
 from __future__ import absolute_import, print_function
 
+import AuthHandler
 import codecs
 import sys
 import tweepy
@@ -12,11 +13,6 @@ from tweepy import Stream
 # specifics to have a bot 
 # TODO put these in seperate file and add to .gitignore
 
-consumer_key="Gra7ryY50P7Wr2oqfjen6C8Q5"
-consumer_secret="vz2IRxjqSUAyiGIqAvFHMtHN3DzcGt3DHl2LnI6zSGwXvP3rqG" 
-access_token="378220962-EZjtEVdg9DVvIt9FqfgiJw38j8iLnAFyc0cGOrnU"
-access_token_secret="RJm3zc30b3hq4TdQLS1WyLW194lLfLINLKqo8zwCsKOfo"
-
 pp = pprint.PrettyPrinter(indent=4)
 
 def store(tweet, profile):
@@ -25,9 +21,7 @@ def store(tweet, profile):
 	fp.write(tweet["text"])
 
 # using OAuth to validate connection
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+api = AuthHandler.login()
 
 # use first argvar for the Twitter ID
 for tweet in tweepy.Cursor(api.user_timeline, id=sys.argv[1]).items():
